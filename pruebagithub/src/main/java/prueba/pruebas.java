b895f3ad7dc1da305b8865c0819979cf78c8a0d8
ivan <ivan@DESKTOP-JQKM9I2.home>
2021-01-17 18:46:18 +0100

    PruebaGit

---
+++ pruebagithub/src/main/java/prueba/pruebas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author ivan
+ */
+public class pruebas {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte numero;
+		numero = 1;
+		while (numero<=100) {
+			if (numero%2==0) {
+				System.out.println(numero);
+			}
+			numero++;
+		}
+			
+	}
+}
